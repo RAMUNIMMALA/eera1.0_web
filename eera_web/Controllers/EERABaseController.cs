5acc23366ca5547175b83841e3b30bd489d6615d
Ramu Nimmala <ramun@goggery.com>
2019-08-12 02:23:29 +0530

    updated config and base controller with image paths

--- eera_web/Controllers/EERABaseController.cs
+++ eera_web/Controllers/EERABaseController.cs
@@ -14,19 +14,54 @@ namespace eera_web.Controllers
         protected string warningMessage = null;
         protected string returnAction = null;
         protected string returnController = null;
+        private string categoryPath ;
+        private string subCategoryPath;
+        private string coursePath;
+        private string institutePath;
+        private string hostelPath;
 
         public EERABaseController()
         {
             categoryPath = Server.MapPath( Convert.ToString(ConfigurationManager.AppSettings["categoryimagepath"]));
+            subCategoryPath = Server.MapPath(Convert.ToString(ConfigurationManager.AppSettings["subcategoryimagepath"]));
+            coursePath = Server.MapPath(Convert.ToString(ConfigurationManager.AppSettings["courseimagepath"]));
+            institutePath = Server.MapPath(Convert.ToString(ConfigurationManager.AppSettings["institutepath"]));
+            hostelPath = Server.MapPath(Convert.ToString(ConfigurationManager.AppSettings["hostelpath"]));
         }
 
         //image paths
-        private string categoryPath;
+        
 
         protected string CategoryPath
         {
             get { return categoryPath; }
             set { categoryPath = value; }
         }
+
+        protected string SubCategoryPath
+        {
+            get { return subCategoryPath; }
+            set { subCategoryPath = value; }
+        }
+
+        protected string CoursePath
+        {
+            get { return coursePath; }
+            set { coursePath = value; }
+        }
+
+        protected string InstitutePath
+        {
+            get { return institutePath; }
+            set { institutePath = value; }
+        }
+
+        protected string HostelPath
+        {
+            get { return hostelPath; }
+            set { hostelPath = value; }
+        }
+
+
     }
 }
