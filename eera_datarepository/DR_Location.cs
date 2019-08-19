96fc6c30fbb728951ac31c708d147445da838804
Ramu Nimmala <ramu.nimmala@cigniti.com>
2019-08-19 23:22:14 +0530

    added locations model and data repository

---
+++ eera_datarepository/DR_Location.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using eera_model;
+
+namespace eera_datarepository
+{
+   public class DR_Location : RepositoryBase
+    {
+        List<Location> lstLocations = null;
+
+        public List<Location> getLocations()
+        {
+            //order in the list should be 
+            //
+            List<string> lstlocationinfo = new List<string>();
+            try
+            {
+                IDbDataParameter[] arrParameter = new IDbDataParameter[] { };
+                dsResultSet = DB_UTILITY.RunSP("USP_FETCH_LOCATION", arrParameter);
+                if (ValidateResultSet(dsResultSet))
+                {
+                    lstLocations = OBJECT_UTILITY.GetConvertCollection<Location>(dsResultSet.Tables[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return lstLocations;
+        }
+    }
+}
---
+++ eera_model/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eera_model
+{
+    public class Location
+    {
+        public int LocationCode { get; set; }
+        public int LocationID { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public int RefCode { get; set; }
+        public bool IsDefault { get; set; }
+        public bool Status { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
